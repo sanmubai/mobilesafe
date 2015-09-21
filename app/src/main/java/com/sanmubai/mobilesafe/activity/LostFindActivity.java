9acc99fd79fccb297fc47323729eb14d5ad204f6
baibin <sanmubai@foxmail.com>
2015-09-21 19:36:02 +0800

    has bug

--- app/src/main/java/com/sanmubai/mobilesafe/activity/LostFindActivity.java
+++ app/src/main/java/com/sanmubai/mobilesafe/activity/LostFindActivity.java
@@ -1,16 +1,31 @@
 package com.sanmubai.mobilesafe.activity;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.sanmubai.mobilesafe.R;
 
 public class LostFindActivity extends Activity {
 
+    private SharedPreferences sp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lost_find);
+
+        //判断是否进入过设置向导，没进过，进入设置向导。否则进入设置后的页面
+        sp=getSharedPreferences("settingConfig", MODE_PRIVATE);
+
+        boolean isSet = sp.getBoolean("isSet", false);
+        if(isSet){
+
+            setContentView(R.layout.activity_lost_find);
+        }else {
+            startActivity(new Intent(this,Setting1Activity.class));
+            finish();
+        }
     }
 
 }
---
+++ app/src/main/java/com/sanmubai/mobilesafe/activity/Setting1Activity.java
@@ -0,0 +1,23 @@
+package com.sanmubai.mobilesafe.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.sanmubai.mobilesafe.R;
+
+public class Setting1Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting1);
+    }
+
+    public void next(View view){
+//        Log.d("test","xia yi bu");
+        startActivity(new Intent(this,Setting2Activity.class));
+        finish();
+    }
+}
